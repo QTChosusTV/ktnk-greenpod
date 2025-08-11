e77e441d141b80f5fdc1bcdc2eb18893778179e5
QTChosusTV <qtchosustv1109@gmail.com>
2025-08-11 19:33:13 +0700

    Sync loading animation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   if (loading) {
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-15 border-green-600"></div>
+        <div className="animate-spin rounded-full h-12 w-12 border-4 border-green-600 border-b-transparent"></div>
       </div>
     );
   }
