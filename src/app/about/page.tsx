1e52847c718c6dbc467eb3d7cb8281ceeadab56a
QTChosusTV <qtchosustv1109@gmail.com>
2025-08-11 19:07:01 +0700

    Init

---
+++ src/app/about/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import '../globals.css';
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 100); 
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-15 border-green-600"></div>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+        
+    </main>
+  );
+}
